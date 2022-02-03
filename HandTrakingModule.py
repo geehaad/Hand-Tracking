8d662356ce8babc5411e4b7a593233b85022a156
geehaad <gehadhisham7@gmail.com>
2022-02-03 20:52:29 +0200

    Update HandTrakingModule.py

--- HandTrakingModule.py
+++ HandTrakingModule.py
@@ -2,18 +2,16 @@ import cv2
 import mediapipe as mp
 import time #to check the frame rate
 
-cap = cv2.VideoCapture(0)
+class handDetector():
+    def __init__(self,mode=False, MaxHands = 2, detectionCon=0.5, trackCon=0.5):
+                 self.mode = mode #object variable
+
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands() #Only uses RGB imgs
 mpDraw = mp.solutions.drawing_utils #Helps us draw lines between the points on the hand
 
-pTime = 0 #Previous time
-cTime = 0 #Current time
 
-
-while True:
-    success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #Convert img to RGB
     results = hands.process(imgRGB)
 
@@ -37,3 +35,26 @@ while True:
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
+def main():
+    pTime = 0  # Previous time
+    cTime = 0  # Current time
+
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, img = cap.read()
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+
+
+if __name__ == "__main__":
+    main()
